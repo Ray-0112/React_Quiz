04eb7893cf7df55222ef1468506a98d5c86e8400
unknown <yuray8901@gmail.com>
2021-04-28 16:58:13 +0800

    modify

--- src/cgu_multiButton.js
+++ src/cgu_multiButton.js
@@ -1,11 +1,13 @@
+import Button from '@material-ui/core/Button';
 const changeText=(event)=>{
     console.log(event.target)
     event.target.innerText = event.target.innerText + "被點了"
    }
-   const MultiButton=(num)=>{
+const MultiButton=(num)=>{
     var output=[];
     for(let i=1;i<num+1;++i)
-    output.push(<button onClick={changeText}>第{i}個按鍵</button>)
+    output.push(<Button variant="contained" color="primary" 
+    onClick={changeText}>第{i}個按鍵</Button>)
     return output;
    }
-   export default MultiButton;
+export default MultiButton;
