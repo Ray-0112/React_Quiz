cbec73c575d66b91044279702fad37a21ca6e650
unknown <yuray8901@gmail.com>
2021-05-12 10:29:30 +0800

    origin

--- src/App.js
+++ src/App.js
@@ -1,18 +1,55 @@
-/*import HelloCGU from './cgu_hello'
-import MultiButton from './cgu_multiButton'
+import React from 'react'
 import BasicTextFields from './login_button'
 import Link from '@material-ui/core/Link';
 import {Grid, TextField} from "@material-ui/core"
 import Lock from "./lock"
 import Button from '@material-ui/core/Button';
-import './App.css';*/
-
-import MultiButton from './changeText'
+import { Card, Icon, Image } from 'semantic-ui-react'
+import './App.css';
 import HelloCGU from './cgu_hello'
+import { useState,useEffect } from 'react';
 
 function App() {
+  const[name,setName] = useState('');
+  const[userName,setUsername] = useState('');
+  const[followers,setFollowers] = useState('');
+  const[following,setFollowing] = useState('');
+  const[repos,setRepos] = useState('');
+  const[avatar,setAvatar] = useState('');
+  const[userInput,setUserInput] = useState('');
+  const[location, setLocation] = useState('');
+  const[create,setJoin] = useState('');
+
+  useEffect(()=>{
+    fetch("https://api.github.com/users/Ray-0112")
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        setData(data)
+      });
+  }, []);
+
+  const setData = ({ 
+    name,
+    login, 
+    followers,
+    following,
+    public_repos,
+    avatar_url,
+    created_at
+  }) =>{
+    setName(name);
+    setUsername(login);
+    setFollowers(followers);
+    setFollowing(following);
+    setRepos(public_repos);
+    setAvatar(avatar_url);
+    setJoin(created_at);
+  };
+
   return (
-    /*<div className="App">
+    <div className="App">
+
         <Grid container style={{minHeight: '100vh'}}>
           <Grid item xs={1} sm={6}>
             <img 
@@ -26,28 +63,46 @@ function App() {
           style={{padding:10}}>
             <div style={{display:"flex",flexDirection:"column",maxHeight:400,maxWidth:300}}>
               <Grid container justify="center">
-              <div style={{marginTop:200}}></div>
-                {HelloCGU()}
-                <div style={{height:200}}></div>
-                  {BasicTextFields()} 
-                <div style={{height:10}}></div>
-                {Lock()}
-                <br></br>
-                <div style={{height:100}}></div>
-                <Button>Forget password?</Button>
-                <Button color="primary" variant="contained" style={{width:900,height:40,marginTop:20}}>
-                  log
-                </Button>
+                <div className="card">
+                <Card>
+                  <Image src={avatar} wrapped ui={false} />
+                  <Card.Content>
+                  <Card.Header>{name}</Card.Header>
+                  <Card.Header>{userName}</Card.Header>
+                    <Card.Meta>
+                      <span className='date'>{create}</span>
+                    </Card.Meta>
+                    <Card.Description>
+                      HI, I'm Ray.
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a>
+                      <Icon name='user' />
+                      {followers} Followers
+                    </a>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a>
+                      <Icon name='user' />
+                      {repos} Repos
+                    </a>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <a>
+                      <Icon name='user' />
+                      {following} Followings
+                    </a>
+                  </Card.Content>
+                </Card>
+                </div>
               </Grid>
             </div>
       
           </Grid>
-        </Grid>*/
+        </Grid>
+        </div>
       
-    <div className="App">
-      {HelloCGU()}
-      {MultiButton(10)}
-    </div>
   );
 }
 
